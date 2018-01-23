0c27d54ad7ea4e1bf149a1dba924791af5639f40
Vison Li <vison.li@oath.com>
2018-01-23 18:58:20 +0800

    add apis

---
+++ app/mapping.js
@@ -0,0 +1,61 @@
+var mapping = {
+    "14103": {
+        "en":"Fine Art Museum",
+        "zh": "國立美術館"
+    },
+    "14104": {
+        "en":"LanYang Museum",
+        "zh":"蘭陽博物館"
+    },
+    "14105": {
+        "en":"Palace Museum",
+        "zh":"故宮"
+    },
+    "14106": {
+        "en":"Taipei 101",
+        "zh":"台北101"
+    },
+    "14109": {
+        "en":"Taipei Arena",
+        "zh":"台北小巨蛋"
+    },
+    "14111": {
+        "en":"Metro Opera House",
+        "zh":"大都會歌劇院"
+    },
+    "14114": {
+        "en":"Science Museum",
+        "zh":"科博館"
+    },
+    "14116": {
+        "en":"85 Building",
+        "zh":"85大樓"
+    },
+    "14703": {
+        "en":"Grand Hotel",
+        "zh":"圓山飯店"
+    },
+    "14711": {
+        "en":"Chungsan Hall",
+        "zh":"中山樓"
+    },
+    "14712": {
+        "en":"CKS Memorial Hall",
+        "zh":"中正紀念堂"
+    },
+    "14715": {
+        "en":"Central Bank",
+        "zh":"中央銀行"
+    },
+    "14721": {
+        "en":"UK Consulate",
+        "zh":"英國領事館"
+    },
+    "14725": {
+        "en":"SYS Memorial Hall",
+        "zh":"國父紀念館"
+    }
+}
+
+
+module.exports = mapping;
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+var mapping = require('../app/mapping');
 var router = express.Router();
 
 router.get('/order', function(req, res) {
@@ -16,4 +18,83 @@ router.post('/order', function(req, res) {
     });
 })
 
+
+/// 
+var mapData = [];
+let roomMapping = new Object();
+
+var elevator = { // middle point between SYS Memorial Hall and Metro Opera House
+    x: 154.67,
+    y: 133
+}
+var startingPoints = {
+    elevator
+}
+
+//"x":492.82, "y":331.8, "x":647.49, "y":464.8,
+
+function init() {
+    const page = "https://search.corp.yahoo.com/map/mapdata.php";
+    var mapid = "784_TPE05_TPE05_14";
+
+    var url = page + "?mapid=" + mapid;
+    request(url, function(err, res, body) {
+        
+        let json = JSON.parse(body);
+        labels = json.d[2].texts;
+        names = json.d[3].texts;
+
+        for(i=0; i<names.length; i++) {
+            var label = json.d[2].texts[i].t;
+            var name = json.d[3].texts[i].t;
+            mapData[i] = {
+                seq: labels[i].t,
+                name: {
+                  en: mapping[label + ""].en,
+                  zh: mapping[label + ""].zh
+                },
+                x: labels[i].x,
+                y: labels[i].y
+            }
+        }
+
+
+        for(i = 0; i<json.d[3].texts.length;i++) {
+            var label = json.d[2].texts[i].t;
+            var name = json.d[3].texts[i].t;
+            
+            roomMapping[label + ""] = name;
+        }
+    });
+}
+
+init();
+
+/**
+ * Get all locations in floor 14th
+ */
+router.get('/maps/floor14', function(req, res) {
+    res.json({
+        "data": mapData
+    });
+})
+
+/**
+ * Get location of starting point
+ */
+router.get('/maps/starting_points', function(req, res) {
+    res.json(elevator);
+})
+
+
+router.get('/maps/floor14_mapping', function(req, res) {
+    res.json({
+        "info": "Data about floor 14 will show here.",
+        "data": roomMapping
+    });
+})
+
+
+
 module.exports = router;
+
